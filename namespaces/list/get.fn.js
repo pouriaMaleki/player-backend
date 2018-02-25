f1add8dc1386d8b706b0d969308b92d7a76b1afb
Pouria Maleki <dev.maleki@gmail.com>
2018-02-26 02:46:36 +0330

    remove log

--- namespaces/list/get.fn.js
+++ namespaces/list/get.fn.js
@@ -1,8 +1,6 @@
 const rp = require('request-promise');
 
 module.exports = function (params) {
-  console.log(params);
-  
   return rp("http://www.getsongg.com/dapp/getnewcases?lang=en&page=" + params.page)
   .then((res) => {
     let result = JSON.parse(res);
