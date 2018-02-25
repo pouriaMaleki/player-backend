f5af6356e196577960c0261f4ac574107edca188
Pouria Maleki <dev.maleki@gmail.com>
2018-02-26 01:55:20 +0330

    support getting more songs

--- namespaces/list/get.fn.js
+++ namespaces/list/get.fn.js
@@ -1,7 +1,9 @@
 const rp = require('request-promise');
 
 module.exports = function (params) {
-  return rp("http://www.getsongg.com/dapp/getnewcases?lang=en")
+  console.log(params);
+  
+  return rp("http://www.getsongg.com/dapp/getnewcases?lang=en&page=" + params.page)
   .then((res) => {
     let result = JSON.parse(res);
     let songs = result.filter((item) => {
